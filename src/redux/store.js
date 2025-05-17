30fc9f76c980924ec936eaf55a3da69c2a3ac766
M.Reza Osfoori <m.osfoori@vandabg.com>
2025-05-17 17:53:13 +0330

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+// import { configureStore } from '@reduxjs/toolkit'
+
+// const CreateAppStore =async () => {
+//  try {
+//     const store=configureStore({
+//         reducer:
+//     })
+//  } catch (error) {
+    
+//  }a
+// }
+
+// export default CreateAppStore
